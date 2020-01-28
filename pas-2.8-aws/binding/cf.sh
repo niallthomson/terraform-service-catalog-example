c034a6bbd2f38ede79d8760540119f2a25dfd977
Niall Thomson <nthomson@pivotal.io>
2020-01-27 18:50:17 -0800

    Need to download jq

--- pas-2.8-aws/binding/cf.sh
+++ pas-2.8-aws/binding/cf.sh
@@ -4,7 +4,9 @@ set -e
 
 wget -O om -q https://github.com/pivotal-cf/om/releases/download/4.4.1/om-linux-4.4.1 && chmod +x om
 
-eval "$(jq -r '@sh "opsman_host=\(.opsman_host) opsman_password=\(.opsman_password)"')"
+wget -O jq -q https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod +x jq
+
+eval "$(./jq -r '@sh "opsman_host=\(.opsman_host) opsman_password=\(.opsman_password)"')"
 
 export OM_TARGET=$opsman_host
 export OM_USERNAME=admin
@@ -12,4 +14,4 @@ export OM_PASSWORD=$opsman_password
 
 cf_password=$(./om credentials -p cf -c .uaa.admin_credentials -t json | jq -r '.password')
 
-jq -n --arg cf_password "$cf_password" '{"cf_password":$cf_password}'
+./jq -n --arg cf_password "$cf_password" '{"cf_password":$cf_password}'
